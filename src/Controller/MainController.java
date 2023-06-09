d13100ecd65a62ad68611474433e475b10d605a8
Lars van Herwijnen <77294984+larsvanherwijnen@users.noreply.github.com>
2023-06-09 14:57:30 +0200

    🍱 Runned the project in eclipse

---
+++ src/Controller/MainController.java
@@ -0,0 +1,22 @@
+package Controller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class MainController extends Application {
+
+
+    public void run(final String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("SpeedQZ - Lars van Herwijnen - PROG4 - 2023");
+
+        stage.show();
+    }
+    
+}
