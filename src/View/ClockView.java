6c26969ec8eb1182111227bc9fb93f15419068aa
Lars van Herwijnen <77294984+larsvanherwijnen@users.noreply.github.com>
2023-06-11 20:56:19 +0200

    ✨ Update clockview

--- src/View/ClockView.java
+++ src/View/ClockView.java
@@ -1,11 +1,13 @@
 package View;
 
 import Model.ClockModel;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class ClockView extends Pane {
     private Label clockLabel;
@@ -15,8 +17,9 @@ public class ClockView extends Pane {
         this.background = new Background(new BackgroundFill(Color.PURPLE, null, null));
         this.setBackground(this.background);
         clockLabel = new Label();
+        clockLabel.setFont(Font.font(72));
         clockLabel.textProperty().bind(clockModel.countdownSecondsProperty().asString());
-
+        clockLabel.setPadding(new Insets(30));
         this.getChildren().add(getLabel());
     }
 
